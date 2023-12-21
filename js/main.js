6f4c6370cdd194d543e34b534a985d492500c07a
ariannacostanzo <costanzo.arianna@outlook.it>
2023-12-21 18:55:41 +0100

    implementing exercise 2 logic

--- js/main.js
+++ js/main.js
@@ -3,41 +3,45 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Stampiamo in pagina i due tiri e il risultato */ 
 
-const startDiceButton = document.getElementById('dadi-start');
-const myNumberText = document.getElementById('my-number-text');
-const cpuNumberText = document.getElementById('cpu-number-text');
-const winner = document.getElementById('winner');
-const youWin = document.getElementById('you-win');
-const cpuWin = document.getElementById('cpu-win');
+//Raccolgo tutti gli elementi che mi servono
+// const startDiceButton = document.getElementById('dadi-start');
+// const myNumberText = document.getElementById('my-number-text');
+// const cpuNumberText = document.getElementById('cpu-number-text');
+// const winner = document.getElementById('winner');
+// const youWin = document.getElementById('you-win');
+// const cpuWin = document.getElementById('cpu-win');
 
-startDiceButton.addEventListener('click', function(){
-    const myNumber = Math.floor(Math.random() * (6 + 1 - 1)) + 1;
+// startDiceButton.addEventListener('click', function(){
+//     //creo numeri casuali da 1 a 6 per user e cpu e li stampo in pagina
+//     const myNumber = Math.floor(Math.random() * (6 + 1 - 1)) + 1;
 
-    myNumberText.innerHTML = `Hai tirato: <strong>${myNumber}<strong>`;
+//     myNumberText.innerHTML = `Hai tirato: <strong>${myNumber}<strong>`;
 
-    const cpuNumber = Math.floor(Math.random() * (6 + 1 - 1)) + 1;
+//     const cpuNumber = Math.floor(Math.random() * (6 + 1 - 1)) + 1;
 
-    cpuNumberText.innerHTML = `CPU ha tirato: <strong>${cpuNumber}<strong>`;
+//     cpuNumberText.innerHTML = `CPU ha tirato: <strong>${cpuNumber}<strong>`;
 
-    let winnerText = ''
+//     //Logica del vincitore
 
-    if (myNumber > cpuNumber) {
-        winnerText = 'Hai vinto!';
-        youWin.classList.remove('d-none');
-        cpuWin.classList.add('d-none');
-    }   else if (myNumber < cpuNumber) {
-        winnerText = 'Ha vinto la cpu!';
-        youWin.classList.add('d-none');
-        cpuWin.classList.remove('d-none');
-    } else {
-        winnerText= 'Pari!';
-        youWin.classList.add('d-none');
-        cpuWin.classList.add('d-none');
-    }
+//     let winnerText = ''
 
-    winner.innerText = winnerText;
+//     if (myNumber > cpuNumber) {
+//         winnerText = 'Hai vinto!';
+//         youWin.classList.remove('d-none');
+//         cpuWin.classList.add('d-none');
+//     }   else if (myNumber < cpuNumber) {
+//         winnerText = 'Ha vinto la cpu!';
+//         youWin.classList.add('d-none');
+//         cpuWin.classList.remove('d-none');
+//     } else {
+//         winnerText= 'Pari!';
+//         youWin.classList.add('d-none');
+//         cpuWin.classList.add('d-none');
+//     }
 
-});
+//     winner.innerText = winnerText;
+
+// });
 
 
 
@@ -49,3 +53,21 @@ Inventa una lista di email autorizzate
 Chiedi all’utente la sua email, con un piccolo form.
 controlla che la mail inserita sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo. */ 
+
+
+//Recupero gli elementi che mi servono
+const userEmail = document.getElementById('email-address');
+const accessButton = document.getElementById('accedi');
+
+// Lista di email autorizzate
+const emails = [
+    'pincopallinojoe@outlook.it',
+    'nonsainientejonsnow@outlook.it',
+];
+
+
+
+
+for (let i = 0; i < emails.length; i++) {
+    console.log(emails[i])
+}
