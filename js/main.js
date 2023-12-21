e2d357adae454604e3d91127fcd0b0e58841c238
ariannacostanzo <costanzo.arianna@outlook.it>
2023-12-21 18:02:12 +0100

    added outline

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* ESERCIZIO 1 - Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampiamo in pagina i due tiri e il risultato */ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ESERCIZIO 2 - Finta Login
+Inventa una lista di email autorizzate
+Chiedi all’utente la sua email, con un piccolo form.
+controlla che la mail inserita sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo. */ 
